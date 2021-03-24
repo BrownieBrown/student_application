27c509a558be3a2a4af076b3485c0105fd0e3952
BrownieBrown <61498988+BrownieBrown@users.noreply.github.com>
2021-03-24 15:13:28 +0100

    included frontend

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,11 +1,124 @@
+import { getAllStudents } from './client'
 import { useState, useEffect } from 'react'
-import { Button } from 'antd'
+import { Layout, Menu, Breadcrumb, Empty, Table, Spin } from 'antd'
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons'
+
+import './App.css'
+
+const { Header, Content, Footer, Sider } = Layout
+const { SubMenu } = Menu
+
+const columns = [
+  {
+    title: 'Id',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+  },
+  {
+    title: 'Gender',
+    dataIndex: 'gender',
+    key: 'gender',
+  },
+]
 
 function App() {
+  const [students, setStudents] = useState([])
+  const [isCollapsed, setIsCollapsed] = useState(false)
+  const [isFetching, setIsFetching] = useState(true)
+
+  const fetchStudents = () => {
+    getAllStudents()
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setStudents(data)
+        setIsFetching(false)
+      })
+  }
+  useEffect(() => {
+    fetchStudents()
+  }, [])
+
+  const renderStudents = () => {
+    if (isFetching) {
+      return <Spin />
+    }
+    if (students.length <= 0) {
+      return <Empty />
+    }
+    return (
+      <Table
+        dataSource={students}
+        columns={columns}
+        bordered
+        title={() => 'Students'}
+        pagination={{ pageSize: 50 }}
+        scroll={{ y: 240 }}
+        rowKey={(student) => {
+          return student.id
+        }}
+      />
+    )
+  }
+
   return (
-    <div className='App'>
-      <Button type='primary'>Hello</Button>
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={isCollapsed} onCollapse={setIsCollapsed}>
+        <div className='logo' />
+        <Menu theme='dark' defaultSelectedKeys={['1']} mode='inline'>
+          <Menu.Item key='1' icon={<PieChartOutlined />}>
+            Option 1
+          </Menu.Item>
+          <Menu.Item key='2' icon={<DesktopOutlined />}>
+            Option 2
+          </Menu.Item>
+          <SubMenu key='sub1' icon={<UserOutlined />} title='User'>
+            <Menu.Item key='3'>Tom</Menu.Item>
+            <Menu.Item key='4'>Bill</Menu.Item>
+            <Menu.Item key='5'>Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu key='sub2' icon={<TeamOutlined />} title='Team'>
+            <Menu.Item key='6'>Team 1</Menu.Item>
+            <Menu.Item key='8'>Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key='9' icon={<FileOutlined />}>
+            Files
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className='site-layout'>
+        <Header className='site-layout-background' style={{ padding: 0 }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className='site-layout-background'
+            style={{ padding: 24, minHeight: 360 }}
+          >
+            {renderStudents()}
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>BrownieBytes ©2021</Footer>
+      </Layout>
+    </Layout>
   )
 }
 export default App
