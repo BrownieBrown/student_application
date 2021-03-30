3debad38eb58d6e5f7dcc9bcc27062f0cb4281a1
BrownieBrown <61498988+BrownieBrown@users.noreply.github.com>
2021-03-30 18:52:45 +0200

    added postgres database and default values for Student model

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("org.postgresql:postgresql")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/mbraun/student_application/model/Student.kt
+++ src/main/kotlin/mbraun/student_application/model/Student.kt
@@ -1,36 +1,18 @@
 package mbraun.student_application.model
 
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+
+@Entity
+@Table
 data class Student (
-    val id: Long,
-    val name: String,
-    val email: String,
-    val gender: Gender) {
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Student
-
-        if (id != other.id) return false
-        if (name != other.name) return false
-        if (email != other.email) return false
-        if (gender != other.gender) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id.hashCode()
-        result = 31 * result + name.hashCode()
-        result = 31 * result + email.hashCode()
-        result = 31 * result + gender.hashCode()
-        return result
-    }
-
-    override fun toString(): String {
-        return "Student(id=$id, name='$name', email='$email', gender=$gender)"
-    }
+    @Id
+    val id: Long = -1,
+    val name: String = "",
+    val email: String = "",
+    val gender: Gender = Gender.OTHER)
 
 
-}
+
