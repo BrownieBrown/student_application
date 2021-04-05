28d7902278660eb94e3c57544d4137753aba6357
BrownieBrown <61498988+BrownieBrown@users.noreply.github.com>
2021-04-05 14:33:37 +0200

    added unit tests

---
+++ src/test/kotlin/mbraun/student_application/repository/StudentRepositoryTest.kt
@@ -0,0 +1,4 @@
+package mbraun.student_application.repository
+
+interface StudentRepository  {
+}
---
+++ src/test/kotlin/mbraun/student_application/service/StudentServiceTest.kt
@@ -0,0 +1,24 @@
+package mbraun.student_application.service
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class StudentServiceTest {
+
+    @Test
+    fun getAllStudents() {
+    }
+
+    @Test
+    fun addStudent() {
+    }
+
+    @Test
+    fun deleteStudent() {
+    }
+
+    @Test
+    fun getStudentRepository() {
+    }
+}
